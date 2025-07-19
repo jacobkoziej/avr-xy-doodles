ebf63b2986a74b157624de944815c29463f89236
Jacob Koziej <jacobkoziej@gmail.com>
2025-07-19 12:08:32 -0400

    feat: add Nix flake

---
+++ flake.nix
@@ -0,0 +1,61 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+{
+  description = "waveform doodles, the AVR way";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    inputs:
+
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+        };
+
+        inherit (pkgs) lib;
+        inherit (pkgs) python3;
+
+        python3-pkgs = python3.withPackages (
+          ps: with ps; [
+            mdformat-simple-breaks
+          ]
+        );
+
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC (
+          let
+            pre-commit-bin = "${lib.getBin pkgs.pre-commit}/bin/pre-commit";
+
+          in
+          {
+            packages = with pkgs; [
+              commitlint-rs
+              mdformat
+              pre-commit
+              python3-pkgs
+              shfmt
+              statix
+              toml-sort
+              treefmt
+              yamlfmt
+              yamllint
+            ];
+
+            shellHook = ''
+              ${pre-commit-bin} install --allow-missing-config > /dev/null
+            '';
+          }
+        );
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
