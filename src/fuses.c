f961a80a90b335d6c523668742bf60fae1ec457c
Jacob Koziej <jacobkoziej@gmail.com>
2025-08-09 08:19:12 -0400

    feat: configure fuses

---
+++ src/fuses.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * fuses.c -- fuse configuration
+ * Copyright (C) 2025  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#include <avr/io.h>
+
+FUSES = {
+	.low	  = 0xFF,
+	.high	  = FUSE_BOOTRST & FUSE_SPIEN,
+	.extended = FUSE_BODLEVEL1,
+};
